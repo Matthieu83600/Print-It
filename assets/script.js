7a6b412651ce13d81b17fa3bf0e3aba1399383f5
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2023-05-25 02:34:04 +0200

    Etape 4-5 : ajout des fonctions liés au carrousel et aux flèches pour faire défiler les images et les descriptifs associés

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Tableau contenant les images et descriptifs 
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -16,31 +17,72 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+// Déclaration des variables 
+let count = 0;
 
-// Création des flèches + test 
+//Eléments HTML modifiés 
+const bannerSlides = document.querySelector(".banner-img");
+const bannerTagLine = document.querySelector("#banner p");
+
+// Flèches 
 const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
 
+// Bullets points
+const dotsSlides = document.querySelector(".dots");
+
+// Création des flèches + test étape 2
 arrowLeft.addEventListener("click", function() {
 	console.log("Le clic de la flèche de gauche fonctionne");
 });
-
-const arrowRight = document.querySelector(".arrow_right");
 
 arrowRight.addEventListener("click", function(){
 	console.log("Le clic de la flèche de droite fonctionne");
 });
 
 
-// Création des bullet points
-const dotsSlides = document.querySelector(".dots");
-
+// Création des bullet points étape 3
 for (let i = 0; i < slides.length; i++) {
 	let dot = document.createElement('div')
 	dot.classList.add("dot")
 	dotsSlides.appendChild(dot);
 }
-let count = 0;
 
 let dotSelected = document.querySelectorAll(".dot");
 dotSelected[count].classList.add("dot_selected");
 
+/**  Création de la fonction pour ajouter toutes les images et descriptifs
+du futur carrousel étape 4-5 **/
+// Création du carrousel 
+
+function showSlide() {
+	bannerSlides.src = `./assets/images/slideshow/${slides[count].image}`;
+	bannerTagLine.innerHTML = slides[count].tagLine;
+}
+	
+// Création de la navigation avec les flèches
+// Flèche de gauche
+function previousSlide() {
+	dotSelected[count].classList.remove(".dot_selected");
+	if (count <= 0) {
+		count = slides.length - 1;
+	} 
+	else {
+		count--;		
+	}	
+	dotSelected[count].classList.add("dot_selected");
+	showSlide()
+}
+
+//Flèche de droite
+function nextSlide() {
+	dotSelected[count].classList.remove(".dot_selected");
+	if (count >= slides.length - 1) {
+		count = 0;
+	} 
+	else {
+		count++;		
+	}	
+	dotSelected[count].classList.add("dot_selected");
+	showSlide()
+}
